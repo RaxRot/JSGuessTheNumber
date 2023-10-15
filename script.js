6d0809b8bdbb63f02c45c60a1146aca112cc710a
Vladosss <dasistperfektosss@gmail.com>
2023-10-15 21:14:22 +0200

    Refactor

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 'use strict';
 
-/*
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = 'Correct Number';
-
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 111;
-*/
-
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 let canPlay = true;
